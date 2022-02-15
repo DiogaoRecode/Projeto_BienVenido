6e3f96ec132f70c0613dd24d31a7d53887dc0554
Diogo Ramalho <diogoeng.mobilidade@gmail.com>
2022-02-15 16:46:27 -0300

    first commit

---
+++ ApiBienVenido_BackEnd/src/test/java/com/bienvenido/ApiBienVenido/ApiBienVenidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bienvenido.ApiBienVenido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBienVenidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
